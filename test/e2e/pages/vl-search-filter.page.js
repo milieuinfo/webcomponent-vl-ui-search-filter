120b1a95acde230322be277af3b7197f4a93369a
pascalriquier <pascalriquier@hotmail.com>
2020-03-03 16:11:15 +0100

    E2E tests voor search-filter (#12)

--- test/e2e/pages/vl-search-filter.page.js
+++ test/e2e/pages/vl-search-filter.page.js
@@ -5,6 +5,14 @@ class VlSearchFilterPage extends Page {
     async _getSearchFilter(selector) {
         return new VlSearchFilter(this.driver, selector);
     }
+    
+    async getSearchFilter() {
+    	return this._getSearchFilter("#search-filter");
+    }
+
+    async getSearchFilterAlt() {
+    	return this._getSearchFilter("#search-filter-alt");
+    }
 
     async load() {
         await super.load(Config.baseUrl + '/demo/vl-search-filter.html');
